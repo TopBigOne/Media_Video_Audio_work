dfbfbef0a31473528b0da60c8bd09ee1a842331b
guangya.zhou <chinagyzhou@gmail.com>
2022-08-31 01:30:23 +0800

    1: 修改编译ffmpeg 脚本！

--- raw_dir/ffmpeg/build_core_ffmpeg.sh
+++ raw_dir/ffmpeg/build_core_ffmpeg.sh
@@ -4,45 +4,44 @@ TOOLCHAIN=/Users/dev/Documents/Android_Env/Sdk/ndk/21.4.7075529/toolchains/llvm/
 # 最低支持的android sdk版本
 API=21
 
-function build_android
-{
-echo "Compiling FFmpeg for $CPU"
-./configure \
- --prefix=$PREFIX \
- --disable-hwaccels \
- --disable-gpl \
- --disable-postproc \
- --enable-shared \
- --enable-jni \
- --disable-mediacodec \
- --enable-small \
- --enable-gpl \
- --disable-decoder=h264_mediacodec \
- --disable-static \
- --disable-doc \
- --disable-programs \
- --disable-ffmpeg \
- --disable-ffplay \
- --disable-ffprobe \
- --disable-avdevice \
- --disable-symver \
- --enable-neon \
- --disable-asm \
- --cross-prefix=$CROSS_PREFIX \
- --target-os=android \
- --arch=$ARCH \
- --cpu=$CPU \
- --cc=$CC \
- --cxx=$CXX \
- --enable-cross-compile \
- --sysroot=$SYSROOT \
- --extra-cflags="-mno-stackrealign -Os $OPTIMIZE_CFLAGS -fPIC" \
- --extra-ldflags="$ADDI_LDFLAGS" \
- $ADDITIONAL_CONFIGURE_FLAG
-make clean
-make -j16
-make install
-echo "The Compilation of FFmpeg for $CPU is completed"
+function build_android {
+  echo "================start compile ffmpeg for: $CPU ======================"
+  ./configure \
+    --prefix=$PREFIX \
+    --disable-hwaccels \
+    --disable-gpl \
+    --disable-postproc \
+    --enable-shared \
+    --enable-jni \
+    --disable-mediacodec \
+    --enable-small \
+    --enable-gpl \
+    --disable-decoder=h264_mediacodec \
+    --disable-static \
+    --disable-doc \
+    --disable-programs \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-avdevice \
+    --disable-symver \
+    --enable-neon \
+    --disable-asm \
+    --cross-prefix=$CROSS_PREFIX \
+    --target-os=android \
+    --arch=$ARCH \
+    --cpu=$CPU \
+    --cc=$CC \
+    --cxx=$CXX \
+    --enable-cross-compile \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-mno-stackrealign -Os $OPTIMIZE_CFLAGS -fPIC" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+  make clean
+  make -j16
+  make install
+  echo "================The Compilation of ffmpeg for $CPU is completed================"
 }
 
 # armv8-a
@@ -55,7 +54,7 @@ CXX=$TOOLCHAIN/bin/aarch64-linux-android$API-clang++
 SYSROOT=$TOOLCHAIN/sysroot
 CROSS_PREFIX=$TOOLCHAIN/bin/aarch64-linux-android-
 # so输出路径
-PREFIX=$(pwd)/android/$CPU
+PREFIX=$(pwd)/android_so_result/arm64-v8a
 OPTIMIZE_CFLAGS="-march=$CPU"
 build_android
 
@@ -73,7 +72,7 @@ CC=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang
 CXX=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang++
 SYSROOT=$TOOLCHAIN/sysroot
 CROSS_PREFIX=$TOOLCHAIN/bin/arm-linux-androideabi-
-PREFIX=$(pwd)/android/$CPU
+PREFIX=$(pwd)/android_so_result/armeabi-v7a
 OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfp -marm -march=$CPU "
 build_android
 
@@ -84,7 +83,7 @@ CC=$TOOLCHAIN/bin/i686-linux-android$API-clang
 CXX=$TOOLCHAIN/bin/i686-linux-android$API-clang++
 SYSROOT=$TOOLCHAIN/sysroot
 CROSS_PREFIX=$TOOLCHAIN/bin/i686-linux-android-
-PREFIX=$(pwd)/android/$CPU
+PREFIX=$(pwd)/android_so_result/$CPU
 OPTIMIZE_CFLAGS="-march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
 build_android
 
@@ -95,7 +94,7 @@ CC=$TOOLCHAIN/bin/x86_64-linux-android$API-clang
 CXX=$TOOLCHAIN/bin/x86_64-linux-android$API-clang++
 SYSROOT=$TOOLCHAIN/sysroot
 CROSS_PREFIX=$TOOLCHAIN/bin/x86_64-linux-android-
-PREFIX=$(pwd)/android/$CPU
+PREFIX=$(pwd)/android_so_result/x86_64
 OPTIMIZE_CFLAGS="-march=$CPU -msse4.2 -mpopcnt -m64 -mtune=intel"
 # 方法调用
 build_android
