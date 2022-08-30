0cbeffd5ee120e2356309794ad3424a747bd398e
guangya.zhou <chinagyzhou@gmail.com>
2022-08-30 23:11:11 +0800

    1: 测试一下ffmpeg 的引入是否成功！；目前看来是ok的！

--- MediaPlayer_One/app/src/main/cpp/native-lib.cpp
+++ MediaPlayer_One/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,35 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+extern "C" {
+#include <libavformat/avformat.h>
+}
+
+
+const char *TAG = "xiao_ya";
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__))
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__))
+
+void ffmpeg_log_output(void *ptr, int level, const char *fmt, va_list vl) {
+    LOGD("[ffmpeg log]%s", fmt);
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_jar_mediaplayer_1one_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+
+    av_log_set_callback(ffmpeg_log_output);
+    AVFormatContext *pContext = avformat_alloc_context();
+    const char *url = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8";
+    avformat_open_input(&pContext, url, nullptr, nullptr);
+    avformat_find_stream_info(pContext, nullptr);
+
     return env->NewStringUTF(hello.c_str());
 }
